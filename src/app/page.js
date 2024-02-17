380cee46676358c9334b6808ac2afcd0b22cd3ce
Najibmansouur <najibmansour2005@gmail.com>
2024-02-17 21:21:50 +0200

    centered carousel on mobile

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import ProductCarousel from "./components/ProductCarousel";
 
 export default function Home() {
   return (
-    <main className="overflow-x-hidden">
+    <main className=" overflow-x-hidden">
       <Section1 />
-      <div className="flex justify-center ">
+      <div className="flex h-screen items-center justify-center ">
         <ProductCarousel />
       </div>
     </main>
