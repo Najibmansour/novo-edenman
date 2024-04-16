9f5d81e80e973851765335b28429a1d150ee6691
Najibmansouur <najibmansour2005@gmail.com>
2024-04-16 23:29:33 +0300

    UPDATE: hero section title on mobile

--- src/components/configedUi/Hero.jsx
+++ src/components/configedUi/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = (props) => {
             <div className="w-[85%] text-white lg:w-[100%] ">
               <div>
                 <div className="flex flex-col ">
-                  <h2 className=" text-3xl lg:text-6xl">
+                  <h2 className=" text-center text-3xl lg:text-left lg:text-7xl">
                     <span className="block ">Dive into</span>
                     <span className="block  font-extrabold ">Aqua Chic</span>
                   </h2>
@@ -27,7 +27,7 @@ const Hero = (props) => {
                 <div className="flex justify-center lg:justify-start lg:pl-5 ">
                   <Link
                     href="https://novo-concepts.com/"
-                    className="custom-button-anim rounded-full bg-white px-6 py-2 text-xs font-extrabold text-gray-600 text-opacity-65  lg:mt-5 lg:px-10 lg:py-3 lg:text-lg "
+                    className="custom-button-anim rounded-full bg-white px-10 py-3 text-sm font-extrabold text-gray-600 text-opacity-65  lg:mt-5 lg:px-10 lg:py-3 lg:text-lg "
                   >
                     SPLASH
                   </Link>
@@ -37,10 +37,10 @@ const Hero = (props) => {
           </div>
 
           <div className="order-1 row-span-2 flex items-end justify-center pb-5 lg:order-2 lg:row-span-2 lg:justify-end">
-            <Flanel className="w-[70%] lg:w-[50%]" />
+            <Flanel className="w-[75%] lg:w-[50%]" />
           </div>
           <div className="order-3 row-span-1 flex items-start justify-center pt-5 lg:order-3 lg:row-span-1 lg:items-start lg:justify-end">
-            <Boxer className=" w-[70%] lg:w-[50%]" />
+            <Boxer className=" w-[75%] lg:w-[50%]" />
           </div>
         </div>
       </div>
