f549572364c7f16ab4e034b7c3cdaaf076b63582
Najibmansouur <najibmansour2005@gmail.com>
2024-05-13 03:24:24 +0300

    FIX: test 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -70,6 +70,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <header>
+        <meta
+          name="google-site-verification"
+          content="1jRpRhjcpV2V-EyD9y2LMwMln-Sll9EsCgBf9n0rn-Q"
+        />
+      </header>
       <body className={inter.className + "relative"}>
         <Toaster
           position="top-center"
