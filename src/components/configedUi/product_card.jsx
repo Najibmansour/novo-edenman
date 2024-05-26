8e9c0bc2beb8245a49fdba9e42779bf3f5d9c6c4
Najibmansouur <najibmansour2005@gmail.com>
2024-05-26 21:33:49 +0300

    UPD: added color variants to products card

--- src/components/configedUi/product_card.jsx
+++ src/components/configedUi/product_card.jsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import { Button } from "../ui/button";
+import ProductColorComponent from "./colorComponent";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const ProductCard = ({
   title,
@@ -35,18 +43,46 @@ const ProductCard = ({
         <p className="mb-4 text-xs  leading-4 text-gray-500 lg:text-sm">
           {description}
         </p>
+
         <div className="mb-4 flex items-center justify-between">
           <span className="text-lg font-bold lg:text-2xl">$49.99</span>
           <Button size="sm " className="px-2 py-1 text-xs">
             Buy
           </Button>
         </div>
-        <div className="flex items-center gap-2">
+        <TooltipProvider delayDuration={0}>
+          <div className="z-50 grid grid-cols-6">
+            {variants.map(({ color, title }, index) => (
+              <Tooltip key={index}>
+                <TooltipTrigger>
+                  {typeof color === "string" ? (
+                    <div
+                      style={{ backgroundColor: `${color}` }}
+                      className="size-[28px] rounded-full border-4 border-neutral-100 lg:size-[42px]"
+                      src={color}
+                      alt={`color-${index}`}
+                    ></div>
+                  ) : (
+                    <div className="size-[28px] rounded-full border-4 border-neutral-100 lg:size-[42px]">
+                      <Image
+                        className="rounded-full"
+                        src={color}
+                        alt={`color-${index}`}
+                      ></Image>
+                    </div>
+                  )}
+                </TooltipTrigger>
+                <TooltipContent>{title}</TooltipContent>
+              </Tooltip>
+            ))}
+          </div>
+        </TooltipProvider>
+        {/* <div className="flex items-center gap-2">
           <div className="h-6 w-6 rounded-full bg-[#000000]" />
           <div className="h-6 w-6 rounded-full bg-[#ffffff]" />
           <div className="h-6 w-6 rounded-full bg-[#0000ff]" />
           <div className="h-6 w-6 rounded-full bg-[#ff00ff]" />
-        </div>
+        </div> */}
       </div>
     </div>
   );
